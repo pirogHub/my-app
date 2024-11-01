e149094580beda3043957221edbd17db234605c2
Bob <bobberbobston@gmail.com>
2024-11-01 03:17:55 +0300

    test-front

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {Header} from './components/header';
+import {fetcher} from './fetch/fake-fetch';
+import {TablePage} from './pages/table-page';
 
-export default App;
+export const App: React.FC = () => {
+	const [routes, setRoutes] = useState<{name: string; link: string}[]>([]);
+	useEffect(() => {
+		fetcher
+			.fetchAvailableStruts()
+			.then((data) => {
+				if (data.data !== null) {
+					setRoutes(
+						data.data.map((route) => {
+							return {
+								name: route,
+								link: `/table/${route.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()}`,
+							};
+						}),
+					);
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
+
+	return (
+		<BrowserRouter>
+			<Header routes={routes} />
+
+			<main className="p-4">
+				<Routes>
+					<Route path="/" element={<div>Choose page</div>} />
+					<Route path="/table/:productName" element={<TablePage />} />
+					<Route path="*" element={<div>NotFound</div>} />
+				</Routes>
+			</main>
+		</BrowserRouter>
+	);
+};
